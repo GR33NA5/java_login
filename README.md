d88e8e11fae08101284e2d70652856470cec4e8e
Moksleivis <Moksleivis@kitm.local>
2023-04-24 10:27:29 +0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # java_login
 # java_login
+# java_login_page
--- app/src/main/java/com/example/aplikacija/MainActivity.java
+++ app/src/main/java/com/example/aplikacija/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.aplikacija;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,10 +19,17 @@ public class MainActivity extends AppCompatActivity {
         password = findViewById(R.id.password);
         loginButton = findViewById(R.id.loginButton);
         loginButton.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
                 if (username.getText().toString().equals("user") && password.getText().toString().equals("1234")) {
                     Toast.makeText(MainActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+                    String username2 = username.getText().toString();
+                    String password2 = password.getText().toString();
+                    Toast.makeText(MainActivity.this, "Username: " + username2 + "\n" + "Password: "
+                    + password2, Toast.LENGTH_SHORT).show();
+                    Intent gotoMainActivity2 = new Intent(MainActivity.this, MainActivity2.class);
+                    startActivity(gotoMainActivity2);
                 } else {
                     Toast.makeText(MainActivity.this, "Login Failed!", Toast.LENGTH_SHORT).show();
                 }
